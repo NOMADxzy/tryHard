7390f536fca08d8c36f79be64c3e9ee83e2e7fe7
NOMADxzy <3302309675@qq.com>
2023-09-11 12:05:34 +0800

    226

---
+++ 递归回溯/226/main.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func invertTree(root *TreeNode) *TreeNode {
+	if root == nil {
+		return nil
+	}
+	root.Left, root.Right = root.Right, root.Left
+	invertTree(root.Left)
+	invertTree(root.Right)
+	return root
+}
+
+func main() {
+	root := &TreeNode{2, &TreeNode{1, nil, nil}, &TreeNode{3, nil, nil}}
+	fmt.Println(invertTree(root))
+}
