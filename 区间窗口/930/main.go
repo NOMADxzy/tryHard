c100baf206a7ff18f2db8bc723ccb30e5b35d79c
NOMAD <3302309675@qq.com>
2023-09-28 00:21:58 +0800

    930

---
+++ 区间窗口/930/main.go
@@ -0,0 +1,37 @@
+package main
+
+import "fmt"
+
+func numSubarraysWithSum(nums []int, goal int) int {
+	l, r := 0, 0
+	cnt := 0
+	sum := 0
+
+	for r < len(nums) {
+		sum += nums[r]
+		for sum > goal && l < len(nums) {
+			sum -= nums[l]
+			l++
+		}
+		if l == len(nums) {
+			break
+		}
+		if sum == goal && r >= l {
+			cnt++
+			for j := l - 1; j >= 0 && nums[j] == 0; j-- {
+				cnt++
+			}
+			for l < r && nums[l] == 0 {
+				cnt++
+				l++
+
+			}
+		}
+		r++
+	}
+	return cnt
+}
+
+func main() {
+	fmt.Println(numSubarraysWithSum([]int{0, 0, 0, 0, 0}, 0))
+}
