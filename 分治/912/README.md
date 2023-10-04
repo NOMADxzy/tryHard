1a7c2ab4ee3a24febb99c2a636b284f67c73bc83
NOMAD <3302309675@qq.com>
2023-10-05 00:39:29 +0800

    912, 归并排序

---
+++ 分治/912/README.md
@@ -0,0 +1,4 @@
+[题目](https://leetcode.cn/problems/sort-an-array/)
+![pic](img.png)
+结果：
+![pic](result.png)
---
+++ 排序/归并排序/main.go
@@ -0,0 +1,43 @@
+package main
+
+import "fmt"
+
+func merge(arr1, arr2 []int) []int {
+	i, j := 0, 0
+	newArr := make([]int, len(arr1)+len(arr2))
+
+	pos := 0
+	for ; i < len(arr1) && j < len(arr2); pos++ {
+		if arr1[i] < arr2[j] {
+			newArr[pos] = arr1[i]
+			i++
+		} else {
+			newArr[pos] = arr2[j]
+			j++
+		}
+	}
+	for ; i < len(arr1); i++ {
+		newArr[pos] = arr1[i]
+		pos++
+	}
+	for ; j < len(arr2); j++ {
+		newArr[pos] = arr2[j]
+		pos++
+	}
+	return newArr
+}
+
+func partitionSort(arr []int) []int {
+	if len(arr) > 1 {
+		mid := len(arr) / 2
+		arr1 := partitionSort(arr[:mid])
+		arr2 := partitionSort(arr[mid:])
+		return merge(arr1, arr2)
+	} else {
+		return arr
+	}
+}
+
+func main() {
+	fmt.Println(partitionSort([]int{5, 1, 1, 2, 0, 0}))
+}
