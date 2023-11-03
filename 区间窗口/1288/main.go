3a0914f79898413110ae95e9557da3486f5073bd
NOMADxzy <3302309675@qq.com>
2023-11-03 22:12:44 +0800

    1296

--- 区间窗口/1288/main.go
+++ 区间窗口/1288/main.go
@@ -16,17 +16,17 @@ func removeCoveredIntervals(intervals [][]int) int {
 	for i := 0; i < len(intervals); i++ {
 		if i > 0 && intervals[i][1] <= rightMax {
 			ans++
-			lastOne = i
-		} else if i > 0 && intervals[i][0] == intervals[i-1][0] && i-1 != lastOne {
+		} else if lastOne >= 0 && intervals[i][0] == intervals[lastOne][0] {
 			ans++
 		}
 		if intervals[i][1] > rightMax {
 			rightMax = intervals[i][1]
+			lastOne = i
 		}
 	}
 	return len(intervals) - ans
 }
 
 func main() {
-	fmt.Println(removeCoveredIntervals([][]int{{1, 4}, {3, 6}, {2, 8}}))
+	fmt.Println(removeCoveredIntervals([][]int{{1, 3}, {2, 4}, {2, 3}, {2, 5}}))
 }
