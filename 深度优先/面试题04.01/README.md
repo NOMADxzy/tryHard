76a46f0216e1819a23c7090c751645eff66da978
NOMADxzy <3302309675@qq.com>
2024-04-13 21:28:31 +0800

    面试题04.01

---
+++ 深度优先/面试题04.01/README.md
@@ -0,0 +1,4 @@
+##### [题目](https://leetcode.cn/problems/check-subtree-lcci/description/![img.png](img.png))
+![pic](img.png)
+##### 结果：
+![pic](result.png)
--- 深度优先/面试题04.01/main.go
+++ 深度优先/面试题04.01/main.go
@@ -1,28 +1,56 @@
 package main
 
-func checkSubTree(t1 *TreeNode, t2 *TreeNode) bool {
-	if t1 == nil && t2 == nil {
-		return true
-	}
-	if t1 == nil || t2 == nil {
-		return false
-	}
-	//如果节点值相等，则需要判断子树完全相等
-	if t1.Val == t2.Val && checkSub(t1.Left, t2.Left) && checkSub(t2.Right, t2.Right) {
-		return true
-	}
-	return checkSubTree(t1.Left, t2) || checkSubTree(t1.Right, t2)
+import "fmt"
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
 }
 
-func checkSub(t1, t2 *TreeNode) bool {
-	if t1 == nil && t2 == nil {
-		return true
+func checkSubTree(t1 *TreeNode, t2 *TreeNode) bool {
+	var dfs func(root *TreeNode) int
+
+	dfs = func(root *TreeNode) int {
+		if root == nil {
+			return 0
+		}
+		cnt := 1 + dfs(root.Left) + dfs(root.Right)
+		return cnt
 	}
-	if t1 == nil || t2 == nil {
+	cnt2 := dfs(t2)
+	var ans bool
+	var compare func(root, node *TreeNode) bool
+	compare = func(root *TreeNode, node *TreeNode) bool {
+		if root == nil {
+			return node == nil
+		}
+		if root.Val != node.Val {
+			return false
+		}
+		if compare(root.Left, node.Left) && compare(root.Right, node.Right) {
+			return true
+		}
 		return false
 	}
-	if t1.Val != t2.Val {
-		return false
+	dfs = func(root *TreeNode) int {
+		if root == nil {
+			return 0
+		}
+		cnt := 1 + dfs(root.Left) + dfs(root.Right)
+		if cnt == cnt2 && compare(root, t2) {
+			ans = true
+		}
+		return cnt
 	}
-	return checkSub(t1.Left, t2.Left) && checkSub(t1.Right, t2.Right)
+	dfs(t1)
+	return ans
+}
+
+func main() {
+	t1 := &TreeNode{1,
+		&TreeNode{2, nil, nil},
+		&TreeNode{2, &TreeNode{1, nil, nil}, nil}}
+	t2 := &TreeNode{2, &TreeNode{1, nil, nil}, nil}
+	fmt.Println(checkSubTree(t1, t2))
 }
